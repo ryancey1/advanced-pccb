5ce0dc83cd0177db4b4a1cc91fea56fce57661a6
Ryan Yancey <ryanjyancey@gmail.com>
2021-07-14 16:44:12 -0700

    Forgot to import jQuery UI

--- unit08/fetch_all.py
+++ unit08/fetch_all.py
@@ -3,6 +3,8 @@
 import mysql.connector
 import json
 
+print("Content-Type: application/json\n\n")
+
 conn = mysql.connector.connect(
     user='ryancey3', password='Rancey19931!', host='localhost', database='biotest'
 )
@@ -12,5 +14,6 @@ curs = conn.cursor()
 curs.execute("SELECT product FROM genes")
 
 print(json.dumps([item for product in curs for item in product]))
+
 curs.close()
 conn.close()
