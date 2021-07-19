de69a3a0c828cc39df8d05cf9988062b43ea0624
Ryan Yancey <ryanjyancey@gmail.com>
2021-07-18 17:41:41 -0700

    Remove old comments, change single to double quote

--- unit08/js/search.js
+++ unit08/js/search.js
@@ -4,12 +4,12 @@ function runSearch() {
     clearResults();
 
     // transforms all the form parameters into a string we can send to the server
-    var frmStr = $('#gene_search').serialize();
+    var frmStr = $("#gene_search").serialize();
 
     // run a search with the input data
     $.ajax({
-        url: './search_product.cgi',
-        dataType: 'json',
+        url: "./search_product.cgi",
+        dataType: "json",
         data: frmStr,
         success: function (data, _textStatus, _jqXHR) {
             processJSON(data);
@@ -26,83 +26,68 @@ function runSearch() {
 // to the result table
 function processJSON(data) {
     // set the span that lists the match count
-    $('#match_count').text(data.match_count);
+    $("#match_count").text(data.match_count);
 
     // this will be used to keep track of row identifiers
     var next_row_num = 1;
 
     // iterate over each match and add a row to the result table for each
     $.each(data.matches, function (i, item) {
-        var this_row_id = 'result_row_' + next_row_num++;
+        var this_row_id = "result_row_" + next_row_num++;
 
         // create a row and append it to the body of the table
-        $('<tr/>', { "id": this_row_id }).appendTo('tbody');
+        $("<tr/>", { "id": this_row_id }).appendTo("tbody");
 
         // add the locus column
-        $('<td/>', { "text": item.locus_id }).appendTo('#' + this_row_id);
+        $("<td/>", { "text": item.locus_id }).appendTo("#" + this_row_id);
 
         // add the product column
-        $('<td/>', { "text": item.product }).appendTo('#' + this_row_id);
+        $("<td/>", { "text": item.product }).appendTo("#" + this_row_id);
 
     });
 
     // now show the result section that was previously hidden
-    $('#results').show();
+    $("#results").show();
 }
 
 // hide results and clear table
 function clearResults() {
-    $('#results').hide();
-    $('tbody').empty();
+    $("#results").hide();
+    $("tbody").empty();
 }
 
 // run our javascript once the page is ready
 $(document).ready(function () {
+
     // autoccomplete function
     // when a key is entered in the search box
     $("#gene_search").keyup(function () {
-        // catch the terms
-        // var term = $('#term').val();
-        // alert(term);
 
+        // run the autocomplete function
         $("#term").autocomplete({
             source: "./autocomplete.cgi",
             minLength: 0
         });
 
-        // submit a POST request to the CGI script with the term
-        // $.ajax({
-        //     url: "./autocomplete.cgi",
-        //     data: term,
-        //     dataType: "json",
-        //     type: "POST",
-        //     success: function (data) {
-        //         // use the return values to autocomplete
-        //         $("#term").autocomplete({
-        //             source: data,
-        //             minLength: 0
-        //         });
-        //     }
-        // });
     });
 
     // when reset button is clicked
-    $('#reset').click(function () {
+    $("#reset").click(function () {
         // clear out search text
-        $('#term').val('');
+        $("#term").val("");
         // focus on text box
-        $('#term').focus();
+        $("#term").focus();
         // hide result table
         clearResults();
-        return false; // prevents 'normal' form submission
+        return false; // prevents "normal" form submission
     });
 
     // define what should happen when a user clicks submit on our search form
-    $('#submit').click(function () {
+    $("#submit").click(function () {
         // search for submitted text
         runSearch();
         // remove focus from search bar
-        $('#term').blur();
-        return false;  // prevents 'normal' form submission
+        $("#term").blur();
+        return false;  // prevents "normal" form submission
     });
 });
